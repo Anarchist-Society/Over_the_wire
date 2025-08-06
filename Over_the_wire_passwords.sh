9410c2947256c937e4a3bd694fd6719cc34fd2d6
Anarchist-Society <220625772+Anarchist-Society@users.noreply.github.com>
2025-08-07 00:40:18 +0200

    Terminado el bandit 13 -> 14

--- Over_the_wire_passwords.sh
+++ Over_the_wire_passwords.sh
@@ -95,3 +95,10 @@ mv data8.bin data8.gz
 gzip -f data8.gz
 
 cat data8 # Esta ya es la contraseña
+
+# level 13 -> level 14: MU4VWeTyJk8ROof1qqmcBPaLh7lDCPvS
+# Para ello nos dan una clave privada para autenticarnos en el usuario bandit14 mediante ssh
+# Hacemoso esto porque la contraseña está en /etc/bandit_pass/bandit14 y solo puede ser leído por el bandit14
+ssh -p 2220 -i ~/sshkey.private bandit14@bandit.labs.overthewire.org # Con el -i se identifica la clave privada
+cat /etc/bandit_pass/bandit14
+
