ccb0921ed870216c3efaebc34e6cda045f41f0d0
Anarchist-Society <220625772+Anarchist-Society@users.noreply.github.com>
2025-08-24 09:14:39 +0200

    17 -> 18

---
+++ Bandit/Level_17_18.sh
@@ -0,0 +1,5 @@
+# Level 17 -> Level 18: x2gLTTjFwMOhQ8oWNbMN362QKxfRqGlO
+# Hay dos ficheros en el home de bandit17: password.old y password.new.
+# La contraseña para el siguiente nivel está en password.new y es la única línea que se ha cambiado entre password.old y password.new.
+# Para este nivel voy a usar el comando diff que compara ficheros línes por línea.
+diff password.new password.old 
